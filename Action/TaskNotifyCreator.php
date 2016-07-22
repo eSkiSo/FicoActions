ad77a45e5d5fada5128695e49f98f4c17fff941b
Manuel Raposo <manuel.raposo@ficosa.com>
2016-07-22 13:56:25 +0100

    Added action

---
+++ Action/TaskNotifyCreator.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Kanboard\Plugin\FicoActions\Action;
+
+use Kanboard\Model\TaskModel;
+use Kanboard\Action\Base;
+
+/**
+ * Notify Task Creator on Action
+ *
+ * @package action
+ * @author  Manuel Raposo
+ */
+class TaskNotifyCreator extends Base
+{
+    /**
+     * Get automatic action description
+     *
+     * @access public
+     * @return string
+     */
+    public function getDescription()
+    {
+        return t('Notify Task creator when the task is moved to another column or closed');
+    }
+
+    /**
+     * Get the list of compatible events
+     *
+     * @access public
+     * @return array
+     */
+    public function getCompatibleEvents()
+    {
+        return array(
+            TaskModel::EVENT_MOVE_COLUMN,
+            TaskModel::EVENT_CLOSE,
+        );
+    }
+
+    /**
+     * Get the required parameter for the action (defined by the user)
+     *
+     * @access public
+     * @return array
+     */
+    public function getActionRequiredParameters()
+    {
+        return array(
+            'column_id' => t('Column'),
+            'subject' => t('Email subject'),
+        );
+    }
+
+    /**
+     * Get the required parameter for the event
+     *
+     * @access public
+     * @return string[]
+     */
+    public function getEventRequiredParameters()
+    {
+        return array(
+            'task_id',
+            'task' => array(
+                'project_id',
+                'creator_id',
+                'column_id',
+            ),
+        );
+    }
+
+    /**
+     * Execute the action
+     *
+     * @access public
+     * @param  array   $data   Event data dictionary
+     * @return bool            True if the action was executed or false when not executed
+     */
+    public function doAction(array $data)
+    {
+        $user = $this->userModel->getById($data['task']['creator_id']);
+        if (! empty($user['email'])) {
+            $this->emailClient->send(
+                $user['email'],
+                $user['name'] ?: $user['username'],
+                $this->getParam('subject'),
+                $this->template->render('notification/task_create', array(
+                    'task' => $data['task'],
+                    'application_url' => $this->configModel->get('application_url'),
+                ))
+            );
+            return true;
+        }
+    }
+
+    /**
+     * Check if the event data meet the action condition
+     *
+     * @access public
+     * @param  array   $data   Event data dictionary
+     * @return bool
+     */
+    public function hasRequiredCondition(array $data)
+    {
+        return true;
+    }
+}
